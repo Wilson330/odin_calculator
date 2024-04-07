c4168c989ebe1c5f2f008e7540ea1fa1bb7b4aa3
Wilson330 <w05200520@gmail.com>
2024-04-08 01:38:46 +0800

    Update the calculator with complete function

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# odin_calculator
-The Odin Project foundation course: JavaScript part
+# The Odin Project foundation course: JavaScript part
+
+## Project 5: Calculator
+
+It's a practice to build a calculator using HTML, CSS and JavaScript.
+
+I tried to imitate the interface and functions of the calculator on IPhone, but I didn't implement the "AC" to "C" function and the digit transformation in fractional-part (just set toFixed(6)).
+
+To use the calculator, please click [here](https://wilson330.github.io/odin_calculator).
+
--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 const add = function(first, second){
-    return first + second;
+    return (parseInt(first) + parseInt(second)).toString();
 }
 
 const subtract = function(first, second){
@@ -11,34 +11,167 @@ const multiply = function(first, second){
 }
 
 const divide = function(first, second){
-    return first / second;
+    return parseFloat((parseFloat(first) / parseFloat(second)).toFixed(6)).toString();
 }
 
-let firstNum;
-let operator;
-let secondNum;
+let firstNum = "0";
+let operator = "nothing";
+let secondNum = "0";
+let dotted = false;
+let answered = false;
 
 const operate = function(firstNum, operator, secondNum){
     if(operator === "+") return add(firstNum, secondNum);
     else if(operator === "-") return subtract(firstNum, secondNum);
     else if(operator === "*") return multiply(firstNum, secondNum);
-    else return divide(first, second);
+    else if(operator === "/") return divide(firstNum, secondNum);
+    
+    return "0";
 }
 
-const changeDisplay = function(text){
+const newDisplay = function(text){
     let display = document.querySelector(".displayer");
     display.textContent = text;
-    secondNum = parseInt(text);
 }
 
-let nowValue = 0;
+const refreshOperator = function(){
+    let dict = {"+": "#add", "-": "#sub", "*": "#mul", "/": "#div"};
+    let button = document.querySelector(dict[operator]);
+    button.style.backgroundColor = "orange";
+}
 
-let display = document.querySelector(".displayer");
+const markOperator = function(Operator){
+    let dict = {"+": "#add", "-": "#sub", "*": "#mul", "/": "#div"};
+    
+    if(operator !== "nothing"){
+        refreshOperator();
+    }
+    let button = document.querySelector(dict[Operator]);
+    button.style.backgroundColor = "brown";
+}
 
+// handle digit
 let digitList = document.querySelectorAll(".digit");
 
 for(let i = 0;i < digitList.length;i++){
     digitList[i].addEventListener("click", () => {
-        changeDisplay(digitList[i].textContent);
+        if(operator === "nothing"){
+            if(firstNum === "0" || answered === true){
+                firstNum = digitList[i].textContent;
+                newDisplay(firstNum);
+                answered = false;
+            }
+            else{
+                firstNum += digitList[i].textContent;
+                newDisplay(firstNum);
+            }
+        }
+        else{
+            if(secondNum === "0"){
+                secondNum = digitList[i].textContent;
+                newDisplay(secondNum);
+            }
+            else{
+                secondNum += digitList[i].textContent;
+                newDisplay(secondNum);
+            }
+        }
     });
 }
+
+// handle dot
+let dot = document.querySelector(".dot");
+dot.addEventListener("click", () => {
+    if(answered === true){
+        firstNum = "0" + dot.textContent;
+        newDisplay(firstNum);
+        dotted = true;
+        answered = false;
+    }
+    if(dotted === false){
+        if(operator === "nothing"){
+            firstNum += dot.textContent;
+            newDisplay(firstNum);
+            dotted = true;
+        }
+        else{
+            secondNum += dot.textContent;
+            newDisplay(secondNum);
+            dotted = true;
+        }
+    }
+});
+
+// handle operator
+let operatorList = document.querySelectorAll(".operator");
+
+for(let i = 0;i < operatorList.length;i++){
+    operatorList[i].addEventListener("click", () => {
+        markOperator(operatorList[i].textContent);
+        operator = operatorList[i].textContent;
+        dotted = false;
+        answered = false;
+    });
+}
+
+// handle equal
+let equal = document.querySelector("#equal");
+
+equal.addEventListener("click", () => {
+    firstNum = operate(firstNum, operator, secondNum);
+    newDisplay(firstNum);
+    refreshOperator();
+    operator = "nothing";
+    secondNum = "0";
+    answered = true;
+});
+
+// handle clear
+let clear = document.querySelector(".clear");
+
+clear.addEventListener("click", () => {
+    firstNum = "0";
+    newDisplay(firstNum);
+    refreshOperator();
+    operator = "nothing";
+    secondNum = "0";
+    dotted = false;
+});
+
+// handle sign
+let sign = document.querySelector(".sign");
+
+sign.addEventListener("click", () => {
+    if(secondNum === "0"){
+        if(firstNum[0] === "-"){
+            firstNum = firstNum.replace("-", "");
+        }
+        else{
+            firstNum = "-" + firstNum;
+        }
+        newDisplay(firstNum);
+    }
+    else{
+        if(secondNum[0] === "-"){
+            secondNum = secondNum.replace("-", "");
+        }
+        else{
+            secondNum = "-" + secondNum;
+        }
+        newDisplay(secondNum);
+    }
+});
+
+// handle percentage
+let percentage = document.querySelector(".percentage");
+
+percentage.addEventListener("click", () => {
+    if(secondNum === "0"){
+        firstNum = operate(firstNum, "/", "100");
+        newDisplay(firstNum);
+    }
+    else{
+        secondNum = operate(secondNum, "/", "100");
+        newDisplay(secondNum);
+    }
+});
